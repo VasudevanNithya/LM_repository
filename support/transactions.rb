5d05c1dff1501aeefd57790cae0546fd7e68e51e
Nithya Vasudevan <nithya.vasudevan@capitalone.com>
2016-09-05 16:22:14 -0500

    expenditure without donuts

--- support/transactions.rb
+++ support/transactions.rb
@@ -6,20 +6,6 @@ require 'json'
 
 url = 'https://prod-api.level-labs.com/api/v2/core/get-all-transactions'
 
-@payload = '{"args":{
-                    "uid":1110590645,
-                    "password":"25782A6311D94286F440F583A2F57F3E",
-                    "api-token": "AppTokenForInterview",
-                    "json-strict-mode":false,
-                    "json-verbose-response": false
-                    }
-          }'
-
-@headers = '{
-            "accept": "application/json",
-            "content-type": "application/json"
-          }'
-
 begin
     response = RestClient.post(url,
                                {'args'=>{'uid'=>1110590645,
@@ -37,12 +23,14 @@ begin
     @aggregate = 0
     @debits = 0
     @credits = 0
-
+    @donut_debit =0
     elements = transaction_data['transactions'].length
-    puts elements
-
     count = 0
+    number_of_credits = 0
+    number_of_debits = 0
     end_of_month = false
+
+    puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"," "
 
     transaction_data['transactions'].each do |t|
         transaction_time = Time.parse(t['transaction-time']).strftime("%Y-%m")
@@ -55,13 +43,15 @@ begin
           next_transaction_time=transaction_time
         end
 
-        if (next_transaction_time) == transaction_time && (count < elements-1)
+        begin
+        if (next_transaction_time == transaction_time) && (count < elements-1)
           @number_of_days = Date.civil(transaction_year.to_i, transaction_month.to_i,-1).day
           end_of_month = false
-        elsif count == elements-1
+        else (next_transaction_time > transaction_time) || (count == elements-1)
           end_of_month = true
-        else
-          end_of_month = true
+        end
+        rescue RestClient::ExceptionWithResponse => err
+          puts err
         end
 
 #        Assuming transaction date is sorted in ascending order in response
@@ -69,30 +59,55 @@ begin
             transaction_amount = (t['amount'].to_i)
             if transaction_amount>=0
               @credits = @credits + transaction_amount
+              number_of_credits = number_of_credits+1
             else
               @debits = @debits + transaction_amount.abs
+              number_of_debits = number_of_debits+1
             end
-            @aggregate = @credits - @debits
+
+            if (t['merchant'] == 'Krispy Kreme Donuts') || (t['merchant'] == 'Dunkin #336784')
+              @donut_debit = @donut_debit + transaction_amount.abs
+              number_of_donutdebits = number_of_debits-1
+            end
 
             if end_of_month == true
+              puts "Averages for month #{transaction_time} by number of credits/debits"
+              average_credits_bynumber = '%.2f' % ((@credits/number_of_credits).to_i/10000)
+              average_debits_bynumber = '%.2f' % ((@debits/number_of_debits).to_f/10000)
+              puts "Income = $#{average_credits_bynumber}"
+              puts "Expenditure = $#{average_debits_bynumber}"," "
+
+              puts "Averages for month #{transaction_time} over #{@number_of_days} days"
               average_credits = '%.2f' % ((@credits/@number_of_days).to_i/10000)
-              puts "average income for month #{transaction_time} = $#{average_credits}"
               average_debits = '%.2f' % ((@debits/@number_of_days).to_f/10000)
-              puts "average expenditure for month #{transaction_time} = $#{average_debits}"," "
+              puts "Income = $#{average_credits}"
+              puts "Expenditure = $#{average_debits}"," "
+
+              donut_debit_amount = '%.2f' % (((@debits-@donut_debit)/@number_of_days).to_f/10000)
+              puts "Average expenditure for #{transaction_time} over #{@number_of_days} days without donut expense = $#{donut_debit_amount}",""
+              puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"," "
               end_of_month = false
             end
         else
-
+          #Reset values at beginning of new month
           reference_date = (Date.parse(t['transaction-time'])).strftime('%Y-%m')
           @aggregate = 0
           @debits = 0
           @credits = 0
-
+          number_of_credits =0
+          number_of_debits=0
+          @donut_debit = 0
+          average_credits_bynumber = 0
+          average_debits_bynumber = 0
+          donut_debit_amount = 0
+          average_credits = 0
+          average_debits = 0
+          #Redo iteration to calculate totals for first transaction of new month
+          redo
         end
 
     count = count+1
     end
-
 
 rescue RestClient::ExceptionWithResponse => err
     puts err
